273ec4786768e724c4043d4dd5e9e5eff67d1cde
seungjjun <il1uso@naver.com>
2023-10-09 00:16:20 +0900

    add domain model mapping
    - 1. 요구사항 분석과 기본 매핑

---
+++ 자바 ORM 표준 JPA 프로그래밍/playground/src/main/java/com/study/jpa/OrderItem.java
@@ -0,0 +1,40 @@
+package com.study.jpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ORDER_ITEM")
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "ODDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int orderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ 자바 ORM 표준 JPA 프로그래밍/playground/src/main/java/com/study/jpa/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.study.jpa;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
